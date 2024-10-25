e59e6c22cfc701455c2886310024a0878960cbc9
olgatorres <bestlaly@mail.ru>
2024-10-25 21:46:38 +0300

    rename app -> module

--- app/main.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-from app.app import run
-
-if __name__ == '__main__':
-    pygame.init()
-    run()
---
+++ module/main.py
@@ -0,0 +1,9 @@
+import pygame
+
+from module.app import run
+
+
+if __name__ == '__main__':
+    pygame.init()
+    for i in range(2):
+        run()
